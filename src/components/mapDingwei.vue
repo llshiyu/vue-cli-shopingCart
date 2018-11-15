75ff577c30adf24d08635ee2e037dd58fbcebddd
shiyu <shiyu@syswin.com>
2018-11-15 14:47:59 +0800

    map

--- src/components/mapDingwei.vue
+++ src/components/mapDingwei.vue
@@ -89,9 +89,11 @@
       },
       toonDingwei(){
         console.log(toongine.location)
+        alert('in')
         toongine.location.getLocation({
           callback: res => {
             console.log("res: " + res.data);
+            alert('succ')
             //{
           //   "latitude" : 80,    //纬度，浮点数，范围为-90~90，负数表示南纬
           //     "longitude" : 80,   //经度，浮点数，范围为-180~180，负数表示西经
@@ -101,9 +103,9 @@
           //     "name" : '',        //位置名
           //     "address" : ''      //地址的详细说明
           // }
-            this.chosePosition.lat = res.data.latitude
-            this.chosePosition.lng = res.data.longitude
-
+            this.chosePosition.lat = 39.932174
+            this.chosePosition.lng = 116.112205
+alert('res.data.latitude: '+res.data.latitude+' res.data.longitude: '+res.data.longitude)
             this.positionPicker()
             // O: 39.99643
             // P: 116.45801
